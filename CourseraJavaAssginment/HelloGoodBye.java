c3c8de083e1f220ac2d82bd95bb20cd02b7df232
ArshM17 <arshmaknojia5@gmail.com>
2023-02-12 15:53:54 +0530

    Added courseraJavaAssignment

---
+++ CourseraJavaAssginment/HelloGoodBye.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloGoodBye{
+	public static void main(String args[]){
+		StdOut.println("Hello "+args[0]+" "+args[1]);
+		StdOut.println("GoodBye "+args[1]+" "+args[0]);
+	}
+}
